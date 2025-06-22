823210c730c46ebd2415218f7dc29bdb466585e6
minhhoccode111 <minhhoccode111@gmail.com>
2025-06-22 18:25:20 +0700

    exercism: ledger
    remove concurrency, replace manually parse date by using time package

--- exercism-go/ledger/ledger.go
+++ exercism-go/ledger/ledger.go
@@ -5,6 +5,7 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Entry struct {
@@ -59,138 +60,121 @@ func FormatLedger(currency string, locale string, entries []Entry) (string, erro
 		return "", errors.New("")
 	}
 
-	// Parallelism, always a great idea
-	co := make(chan Info)
-	for i, et := range entriesCopy {
-		go func(i int, entry Entry) {
-			if len(entry.Date) != 10 {
-				co <- Info{err: errors.New("")}
-			}
-			d1, d2, d3, d4, d5 := entry.Date[0:4], entry.Date[4], entry.Date[5:7], entry.Date[7], entry.Date[8:10]
-			if d2 != '-' {
-				co <- Info{err: errors.New("")}
-			}
-			if d4 != '-' {
-				co <- Info{err: errors.New("")}
-			}
-			de := entry.Description
-			if len(de) > 25 {
-				de = de[:22] + "..."
-			} else {
-				de = de + strings.Repeat(" ", 25-len(de))
-			}
-			var d string
-			switch locale {
-			case "nl-NL":
-				d = d5 + "-" + d3 + "-" + d1
-			case "en-US":
-				d = d3 + "/" + d5 + "/" + d1
-			}
-			negative := false
-			cents := entry.Change
-			if cents < 0 {
-				cents = cents * -1
-				negative = true
-			}
-			var a string
-			switch locale {
-			case "nl-NL":
-				switch currency {
-				case "EUR":
-					a += "€"
-				case "USD":
-					a += "$"
-				default:
-					co <- Info{err: errors.New("")}
-				}
-				a += " "
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + "."
-				}
-				a = a[:len(a)-1]
-				a += ","
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += "-"
-				} else {
-					a += " "
-				}
-			case "en-US":
-				if negative {
-					a += "("
-				}
-				switch currency {
-				case "EUR":
-					a += "€"
-				case "USD":
-					a += "$"
-				default:
-					co <- Info{err: errors.New("")}
-				}
-				centsStr := strconv.Itoa(cents)
-				switch len(centsStr) {
-				case 1:
-					centsStr = "00" + centsStr
-				case 2:
-					centsStr = "0" + centsStr
-				}
-				rest := centsStr[:len(centsStr)-2]
-				var parts []string
-				for len(rest) > 3 {
-					parts = append(parts, rest[len(rest)-3:])
-					rest = rest[:len(rest)-3]
-				}
-				if len(rest) > 0 {
-					parts = append(parts, rest)
-				}
-				for i := len(parts) - 1; i >= 0; i-- {
-					a += parts[i] + ","
-				}
-				a = a[:len(a)-1]
-				a += "."
-				a += centsStr[len(centsStr)-2:]
-				if negative {
-					a += ")"
-				} else {
-					a += " "
-				}
-			default:
-				co <- Info{err: errors.New("")}
-			}
-			var al int
-			for range a {
-				al++
-			}
-			co <- Info{index: i, str: d + strings.Repeat(" ", 10-len(d)) + " | " + de + " | " +
-				strings.Repeat(" ", 13-al) + a + "\n"}
-		}(i, et)
-	}
-	ss := make([]string, len(entriesCopy))
-	for range entriesCopy {
-		v := <-co
-		if v.err != nil {
-			return "", v.err
+	usFormat := "01/02/2006"
+	nlFormat := "02-01-2006"
+	inputFormat := "2006-01-02"
+	for _, entry := range entriesCopy {
+		date, err := time.Parse(inputFormat, entry.Date)
+		if err != nil {
+			return "", errors.New("Date is not valid")
 		}
-		ss[v.index] = v.str
-	}
-	for i := 0; i < len(entriesCopy); i++ {
-		s += ss[i]
+		desc := entry.Description
+		if len(desc) > 25 {
+			desc = desc[:22] + "..."
+		} else {
+			desc = desc + strings.Repeat(" ", 25-len(desc))
+		}
+		var dateString string
+		switch locale {
+		case "nl-NL":
+			dateString = date.Format(nlFormat)
+		case "en-US":
+			dateString = date.Format(usFormat)
+		default:
+			return "", errors.New("Locale is not valid")
+		}
+		negative := false
+		cents := entry.Change
+		if cents < 0 {
+			cents = cents * -1
+			negative = true
+		}
+		var a string
+		switch locale {
+		case "nl-NL":
+			switch currency {
+			case "EUR":
+				a += "€"
+			case "USD":
+				a += "$"
+			default:
+				return "", errors.New("Currency is not valid")
+			}
+			a += " "
+			centsStr := strconv.Itoa(cents)
+			switch len(centsStr) {
+			case 1:
+				centsStr = "00" + centsStr
+			case 2:
+				centsStr = "0" + centsStr
+			}
+			rest := centsStr[:len(centsStr)-2]
+			var parts []string
+			for len(rest) > 3 {
+				parts = append(parts, rest[len(rest)-3:])
+				rest = rest[:len(rest)-3]
+			}
+			if len(rest) > 0 {
+				parts = append(parts, rest)
+			}
+			for i := len(parts) - 1; i >= 0; i-- {
+				a += parts[i] + "."
+			}
+			a = a[:len(a)-1]
+			a += ","
+			a += centsStr[len(centsStr)-2:]
+			if negative {
+				a += "-"
+			} else {
+				a += " "
+			}
+		case "en-US":
+			if negative {
+				a += "("
+			}
+			switch currency {
+			case "EUR":
+				a += "€"
+			case "USD":
+				a += "$"
+			default:
+				return "", errors.New("Cuerrency is not valid")
+			}
+			centsStr := strconv.Itoa(cents)
+			switch len(centsStr) {
+			case 1:
+				centsStr = "00" + centsStr
+			case 2:
+				centsStr = "0" + centsStr
+			}
+			rest := centsStr[:len(centsStr)-2]
+			var parts []string
+			for len(rest) > 3 {
+				parts = append(parts, rest[len(rest)-3:])
+				rest = rest[:len(rest)-3]
+			}
+			if len(rest) > 0 {
+				parts = append(parts, rest)
+			}
+			for i := len(parts) - 1; i >= 0; i-- {
+				a += parts[i] + ","
+			}
+			a = a[:len(a)-1]
+			a += "."
+			a += centsStr[len(centsStr)-2:]
+			if negative {
+				a += ")"
+			} else {
+				a += " "
+			}
+		default:
+			return "", errors.New("Locale is not valid")
+		}
+		var al int
+		for range a {
+			al++
+		}
+		s += dateString + strings.Repeat(" ", 10-len(dateString)) + " | " + desc + " | " + strings.Repeat(" ", 13-al) + a + "\n"
 	}
 	return s, nil
 }
