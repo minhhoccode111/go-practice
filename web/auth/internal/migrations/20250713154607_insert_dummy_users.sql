5bf1910bef21ee3c86ac54020e2b5dd7cf211faa
minhhoccode111 <minhhoccode111@gmail.com>
2025-07-13 23:18:21 +0700

    add(web/auth): goose database migrations and Makefile

--- web/auth/internal/migrations/20250713154607_insert_dummy_users.sql
+++ web/auth/internal/migrations/20250713154607_insert_dummy_users.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 INSERT INTO users (email, password, last_name, first_name, is_active, role)
 SELECT * FROM (VALUES
-  ('minhhoccode111@gmail.com', '$2a$10$I9ZdFZ1OMx.LO3dnmv65DO344FPoaUj8LXXv01jzmIgIKFAqF5uia', 'Minh', 'Dang', true, 'admin'),
-  ('asd@gmail.com', '$2a$10$I9ZdFZ1OMx.LO3dnmv65DO344FPoaUj8LXXv01jzmIgIKFAqF5uia', 'Dummy', 'Account', true, 'user')
+  ('minhhoccode111@gmail.com', '$2a$10$I9ZdFZ1OMx.LO3dnmv65DO344FPoaUj8LXXv01jzmIgIKFAqF5uia', 'Minh', 'Dang', true, 'admin'::user_role),
+  ('asd@gmail.com', '$2a$10$I9ZdFZ1OMx.LO3dnmv65DO344FPoaUj8LXXv01jzmIgIKFAqF5uia', 'Dummy', 'Account', true, 'user'::user_role)
 ) AS t(email, password, last_name, first_name, is_active, role)
 WHERE NOT EXISTS (SELECT 1 FROM users);
 -- +goose StatementEnd
