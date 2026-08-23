207d9dea30ff3996d3ab11f8b2241543ec7603c1
minhhoccode111 <minhhoccode111@gmail.com>
2026-08-23 22:29:01 +0700

    wip: graceful shutdown

--- chore/graceful-shutdown/main.go
+++ chore/graceful-shutdown/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("tai vi sao")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("request to /")
+		sleep := r.URL.Query().Get("sleep")
+		d, err := time.ParseDuration(sleep)
+		if err != nil {
+			d = 0 * time.Second
+		}
+		time.Sleep(d)
+		w.Write([]byte("done\n"))
+	})
+	svr := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  10 * time.Second,
+	}
+	log.Println("starting server at :8080")
+	go func() {
+		if err := svr.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+	ctx := context.Background()
+	svr.Shutdown(ctx)
 }
